cd38c56686309ca0d2b25fb268aeccddab3d2b7a
linhntt011112 <linhntt011199@gmail.com>
2021-09-15 15:00:35 +0700

    dependency injection

--- app/code/SimplifiedMagento/FirstModule/Controller/Page/HelloWorld.php
+++ app/code/SimplifiedMagento/FirstModule/Controller/Page/HelloWorld.php
@@ -2,10 +2,20 @@
 
 namespace SimplifiedMagento\FirstModule\Controller\Page;
 
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\ResponseInterface;
+use SimplifiedMagento\FirstModule\NotMagento\PencilInterface;
+
 class HelloWorld extends \Magento\Framework\App\Action\Action
 {
+    protected $pencilInterface;
+    public function __construct(Context $context, PencilInterface $pencilInterface)
+    {
+        $this->pencilInterface = $pencilInterface;
+        parent::__construct($context);
+    }
     public function execute()
     {
-        echo "Hello World";
+        echo $this->pencilInterface->getPencilType();
     }
 }
