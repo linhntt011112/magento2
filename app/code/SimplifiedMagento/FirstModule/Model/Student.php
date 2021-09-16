856749a36047dba2479c843be7cb29aebf7be6b5
linhntt011112 <linhntt011199@gmail.com>
2021-09-16 14:09:32 +0700

    argument types: array

--- app/code/SimplifiedMagento/FirstModule/Model/Student.php
+++ app/code/SimplifiedMagento/FirstModule/Model/Student.php
@@ -5,9 +5,11 @@ namespace SimplifiedMagento\FirstModule\Model;
 class Student {
     private $name;
     private $age;
-    public function __construct($name = "Alex", $age = 28)
+    private $scores;
+    public function __construct($name = "Alex", $age = 28, array $scores = array('maths' => 92, 'programming' => 90))
     {
         $this->name = $name;
         $this->age = $age;
+        $this->scores = $scores;
     }
 }
