39186d719397aff675f9349bf46573d83ed78018
linhntt011112 <linhntt011199@gmail.com>
2021-09-17 16:04:25 +0700

    before plugin

--- app/code/SimplifiedMagento/FirstModule/Controller/Page/HelloWorld.php
+++ app/code/SimplifiedMagento/FirstModule/Controller/Page/HelloWorld.php
@@ -7,23 +7,30 @@ use Magento\Framework\App\ResponseInterface;
 use SimplifiedMagento\FirstModule\Api\PencilInterface;
 use Magento\Catalog\Api\ProductRepositoryInterface;
 use SimplifiedMagento\FirstModule\Model\PencilFactory;
+use Magento\Catalog\Model\ProductFactory;
 
 class HelloWorld extends \Magento\Framework\App\Action\Action
 {
     protected $pencilInterface;
     protected $productRepository;
     protected $pencilFactory;
-    public function __construct(Context $context, PencilFactory $pencilFactory, PencilInterface $pencilInterface, ProductRepositoryInterface $productRepository)
+    protected $productFactory;
+    public function __construct(Context $context, ProductFactory $productFactory, PencilFactory $pencilFactory, PencilInterface $pencilInterface, ProductRepositoryInterface $productRepository)
     {
         $this->pencilFactory = $pencilFactory;
         $this->pencilInterface = $pencilInterface;
         $this->productRepository = $productRepository;
+        $this->productFactory = $productFactory;
         parent::__construct($context);
     }
     public function execute()
     {
-        $pencil = $this->pencilFactory->create(array("name"=>"Bob", "school"=>"International College"));
-        var_dump($pencil);
+        // $pencil = $this->pencilFactory->create(array("name"=>"Bob", "school"=>"International College"));
+        // var_dump($pencil);
+        $product = $this->productFactory->create()->load(1);
+        $product->setName("Iphone 6");
+        $productName = $product->getName();
+        echo $productName;
     }
 
     
---
+++ app/code/SimplifiedMagento/FirstModule/Plugin/PluginSolution.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SimplifiedMagento\FirstModule\Plugin;
+
+class PluginSolution {
+    public function beforeSetName(\Magento\Catalog\Model\Product $subject, $name) {
+        return "Before Plugin ". $name;
+    }
+}
