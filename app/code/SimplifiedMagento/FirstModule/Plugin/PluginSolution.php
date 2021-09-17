3a6948297c14178fb9aec929eb62943b38470ae4
linhntt011112 <linhntt011199@gmail.com>
2021-09-17 17:22:04 +0700

    before and after plugin

--- app/code/SimplifiedMagento/FirstModule/Plugin/PluginSolution.php
+++ app/code/SimplifiedMagento/FirstModule/Plugin/PluginSolution.php
@@ -6,4 +6,8 @@ class PluginSolution {
     public function beforeSetName(\Magento\Catalog\Model\Product $subject, $name) {
         return "Before Plugin ". $name;
     }
+
+    // public function afterGetName(\Magento\Catalog\Model\Product $subject, $result) {
+    //     return $result ." After Plugin";
+    // }
 }
