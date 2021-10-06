94475c57c72f9ea82b2d078e6fc20fefb41a95e5
linhntt011112 <linhntt011199@gmail.com>
2021-10-06 14:21:35 +0700

    custom no route handler

---
+++ app/code/SimplifiedMagento/RequestFlow/Controller/Page/CustomNoRouteHandler.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace SimplifiedMagento\RequestFlow\Controller\Page;
+
+
+class CustomNoRouteHandler implements \Magento\Framework\App\Router\NoRouteHandlerInterface{
+
+    /**
+     * Check and process no route request
+     *
+     * @param \Magento\Framework\App\RequestInterface $request
+     * @return bool
+     */
+    public function process(\Magento\Framework\App\RequestInterface $request)
+    {
+        
+        $request->setModuleName('noroutefound')->setControllerName('page')->setActionName('customnoroute');
+        return true;
+    }
+}
