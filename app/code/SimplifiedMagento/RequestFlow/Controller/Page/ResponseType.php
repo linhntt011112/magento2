b033f86f1175a60f44e2af55f565d299864f3e21
linhntt011112 <linhntt011199@gmail.com>
2021-10-07 09:42:47 +0700

    response types

--- app/code/SimplifiedMagento/RequestFlow/Controller/Page/ResponseType.php
+++ app/code/SimplifiedMagento/RequestFlow/Controller/Page/ResponseType.php
@@ -9,6 +9,7 @@ use Magento\Framework\View\Result\PageFactory;
 use Magento\Framework\Controller\Result\JsonFactory;
 use Magento\Framework\Controller\Result\Raw;
 use Magento\Framework\Controller\Result\ForwardFactory;
+use Magento\Framework\Controller\Result\RedirectFactory;
 
 class ResponseType extends Action{
 
@@ -16,13 +17,15 @@ class ResponseType extends Action{
     protected $jsonFactory;
     protected $raw;
     protected $forwardFactory;
+    protected $redirectFactory;
 
-    public function __construct(Context $context, PageFactory $pageFactory, JsonFactory $jsonFactory, Raw $raw, ForwardFactory $forwardFactory)
+    public function __construct(Context $context, PageFactory $pageFactory, JsonFactory $jsonFactory, Raw $raw, ForwardFactory $forwardFactory, RedirectFactory $redirectFactory)
     {
         $this->pageFactory = $pageFactory;
         $this->jsonFactory = $jsonFactory;
         $this->raw = $raw;
         $this->forwardFactory = $forwardFactory;
+        $this->redirectFactory = $redirectFactory;
         parent::__construct($context);
     }
     /**
@@ -37,8 +40,11 @@ class ResponseType extends Action{
         // return $this->jsonFactory->create()->setData(['key' => 'value', 'key2' => ['one', 'two']]);
         // $result = $this->raw->setContents('hello world');
         // return $result;
-        $result = $this->forwardFactory->create();
-        $result->setModule('noroutefound')->setController('page')->forward('customnoroute');
+        // $result = $this->forwardFactory->create();
+        // $result->setModule('noroutefound')->setController('page')->forward('customnoroute');
+        // return $result;
+        $result = $this->redirectFactory->create();
+        $result->setPath('noroutefound/page/customnoroute');
         return $result;
     }
 }
